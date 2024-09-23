27a29a8b29f5f4ecadbe54965bf18e56c1657daa
StrBoll <phillipboll107@gmail.com>
2024-09-22 21:54:57 -0700

    POSTGRESQL DATABASE CREATED

--- src/database.sql
+++ src/database.sql
@@ -1,10 +1,7 @@
 CREATE TABLE blocks (
-
-
-
-
-
-
-
+    block_number SERIAL PRIMARY KEY,
+    previous_hash VARCHAR(255),
+    current_hash VARCHAR(255),
+    nonce INT,
+    transaction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
