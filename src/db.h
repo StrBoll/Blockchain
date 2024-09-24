1b1e8ce53b35821081e425540f074c271971477c
StrBoll <phillipboll107@gmail.com>
2024-09-23 22:58:31 -0400

    database functionality implementation

--- src/db.h
+++ src/db.h
@@ -5,7 +5,11 @@
 #include <string> 
 
 
-void blockDB(const std::string &prevHash, const std::string &hash, int nonce, time_t transactions);
+void insertBlockDB(const std::string &prevHash, const std::string &hash, int nonce, time_t transactions);
+
+bool blocksPresent();
+
+bool eraseDB();
 
 
 #endif
