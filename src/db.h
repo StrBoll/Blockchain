8e8bbfaa52f9a04c39c1d70c6bd87ab855fb5949
StrBoll <phillipboll107@gmail.com>
2024-09-24 00:20:09 -0400

    new hashing database functions

--- src/db.h
+++ src/db.h
@@ -5,11 +5,13 @@
 #include <string> 
 
 
-void insertBlockDB(const std::string &prevHash, const std::string &hash, int nonce, time_t transactions);
+void insertBlockDB(const std::string &prevHash, const std::string &hash, int nonce, time_t transactions); // inserts new blocks to database
 
-bool blocksPresent();
+bool blocksPresent(); // checks if blocks are in blockchain 
 
-bool eraseDB();
+bool eraseDB(); // erase everything in database
+
+std::string getLatestHash();
 
 
 #endif
