468cc70cd23f9d26fc17a30f2e2e101bddaefa9e
StrBoll <phillipboll107@gmail.com>
2024-09-23 20:19:35 -0400

    PostgreSQL Function Completed

--- src/db.cpp
+++ src/db.cpp
@@ -1,9 +1,27 @@
-#include "src/db.h"
+#include "db.h"
+#include <pqxx/pqxx> 
 #include <iostream>
+#include <string>
+
+using namespace pqxx;
 using namespace std;
+void blockDB(const std::string& prevHash, const std::string& hash, int nonce, time_t transactions) {
+    try {
+        
+        connection C("dbname=database user=phillipboll3 password=Internship2025 hostaddr=127.0.0.1 port=5432");
+        
+        if (C.is_open()) {
+            work W(C);
+            string sql = "INSERT INTO blocks (previous_hash, current_hash, nonce, transaction_time) "
+            "VALUES ('" + prevHash + "', '" + hash + "', " + to_string(nonce) + ", to_timestamp(" + to_string(transactions) + "));";
 
-void blockDB(const string& prevHash, const string& hash, int nonce, time_t transactions){
+            
+            W.exec(sql);
+            W.commit();
 
+        } 
 
-    
+    } catch (const exception &e) {
+        cerr << e.what() << endl;
+    }
 }
