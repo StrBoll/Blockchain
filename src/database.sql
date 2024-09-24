adf2b2dfbe3692690ba6178415a6e0e6f2ffd922
StrBoll <phillipboll107@gmail.com>
2024-09-24 14:08:18 -0400

    votes table

--- src/database.sql
+++ src/database.sql
@@ -5,3 +5,12 @@ CREATE TABLE blocks (
     nonce INT,
     transaction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE votes (
+    block_number INT PRIMARY KEY,
+    voter_registration INT,
+    voter_name VARCHAR(255),
+    candidate_vote VARCHAR(255),
+    FOREIGN KEY (block_number) REFERENCES blocks(block_number)
+);
