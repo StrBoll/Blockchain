b44dba9c14c3559780c66d4e54eec9bab0455dcb
StrBoll <phillipboll107@gmail.com>
2024-09-27 18:35:43 -0400

    multithreading

---
+++ src/multithread.cpp
@@ -0,0 +1,68 @@
+#include "multithread.h"
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include "openssl/sha.h"
+
+using namespace std;
+
+int Mining::mineBlock(string prevHash, string data, string& returnHash){
+    int nonce = 0; 
+    string temporaryHash; 
+
+    do {
+        string input = prevHash + data + to_string(nonce);
+        temporaryHash = compute256(input);
+        nonce++;
+    } while (!validHash(temporaryHash, difficulty));
+
+    returnHash = temporaryHash;
+
+    cout << "Valid hash found at: " << temporaryHash << endl;
+
+
+    return nonce -1;
+}
+
+string Mining::HexIt(const unsigned char* input, size_t length) {
+    const char* hexAlphabet = "0123456789abcdef";
+    string output;
+    
+    for (size_t i = 0; i < length; i++) {
+        output.push_back(hexAlphabet[(input[i] >> 4) & 0xF]);
+        output.push_back(hexAlphabet[input[i] & 0xF]);       
+    }
+    
+    return output;
+}
+
+bool Mining::validHash(const string& hash){
+    int i = 0;
+    for (auto character : hash){
+        if (character == '0'){
+            i++;
+        }
+        else {
+            break;
+        }
+
+    }
+
+    return i == difficulty;
+}
+
+
+string Mining::compute256(const string& input){
+
+// Deprecated, newer Envelope API is available but since this is just a project I'm doing for learning about blockchain I figured it didn't matter
+   unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, input.c_str(), input.size());
+    SHA256_Final(hash, &sha256);
+
+    
+
+    return HexIt(hash, SHA256_DIGEST_LENGTH);
+}
