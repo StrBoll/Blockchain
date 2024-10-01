e37cfe86b3022c7e5426750aa1d9e6e7fb83a879
StrBoll <phillipboll107@gmail.com>
2024-10-01 19:42:49 -0400

    databasing in terminal with Crows

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,7 @@
 #include "crow.h"
 #include "blockchain.h"
 #include "db.h"
+#include <pqxx/pqxx>
 
 BlockChain blockchain;
 
@@ -41,6 +42,60 @@ int main() {
 
         insertBlockDB(blockchain.getTail()->prevHash, blockchain.getTail()->Hash, blockchain.getTail()->nonce, blockchain.getTail()->transactions);
         insertVoteDB(firstName, lastName, candidate);
+
+        crow::response res;
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Methods", "POST, OPTIONS");
+        res.set_header("Access-Control-Allow-Headers", "Content-Type");
+        res.write("Block added to blockchain and vote saved.");
+        res.code = 200;
+        return res;
+    });
+
+    CROW_ROUTE(app, "/printVotes").methods(crow::HTTPMethod::POST, crow::HTTPMethod::OPTIONS)
+    ([](const crow::request& req) {
+        if (req.method == crow::HTTPMethod::OPTIONS) {
+            crow::response res;
+            res.set_header("Access-Control-Allow-Origin", "*");
+            res.set_header("Access-Control-Allow-Methods", "POST, OPTIONS");
+            res.set_header("Access-Control-Allow-Headers", "Content-Type");
+            res.code = 204;
+            return res;
+        }
+
+        auto body = crow::json::load(req.body);
+        if (!body) {
+            return crow::response(400, "Invalid JSON data");
+        }
+
+        string toTerminal;
+
+        try {
+
+        connection C("dbname=aws_database user=phillipboll3 password='#NewPassword2024' host=localhost port=5432");
+
+            if (C.is_open()){
+                nontransaction N(C);
+
+                string command = "SELECT * FROM votes;";
+
+                result R(N.execute(command));
+
+                const auto iter = R.begin();
+
+                for (iter; iter != R.end(); ++iter){
+                    toTerminal = *iter["voterFirst"].as<string>() + " " + *iter["VoterLast"].as<string>() + " Vote: " << *iter["candidate_vote"].as<string>() + "\n";
+
+                }
+
+            } else {
+                return crow::response(500, "Failed to connect to AWS Database");
+            }
+
+        }
+        catch (const exception &e){
+            cerr << e.what() << endl;
+        }
 
         crow::response res;
         res.set_header("Access-Control-Allow-Origin", "*");
